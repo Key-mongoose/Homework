f7b54c31ca8183f8c2063904c7c906c3b699e3f7
Mongoose <32216804+Key-mongoose@users.noreply.github.com>
2017-09-23 17:13:35 +0800

    递归作业

---
+++ git项目/n2n递归/src/n2n递归/n2n递归.java
@@ -0,0 +1,62 @@
+
+	 /**     
+     * @discription 在此输入一句话描述此文件的作用
+     * @author 陈欢      
+     * @created 2017年9月23日 下午2:19:34    
+     * tags     
+     * see_to_target     
+     */
+    
+package n2n递归;
+import java.util.*;
+  
+    /**        
+ * Title: n2n递归.java    
+ * Description: 不用循环和本地变量，按照顺序输出值
+ * @author 陈欢      
+ * @created 2017年9月23日 下午2:19:34    
+ */
+
+public class n2n递归 {
+
+	/**     
+	 * @discription 一个整数，大于0，不用循环和本地变量，按照n，2n，4n，8n的顺序 递增，当值大于5000时，把值按照指定顺序输出来。
+	 *              例：n=1237
+	 *              输出为：
+	 *              1237
+	 *              2474
+	 *              4948
+	 *              9896
+	 *              9896
+	 *              4948
+	 *              2427
+	 *              1237  
+	 * @author 陈欢       
+	 * @created 2017年9月23日 下午2:19:34     
+	 * @param args     
+	 */
+	private  int num;
+	public static void Num(int a){
+			if (a <= 0)
+			{
+			    System.out.println("输入的数不大于0！！！");
+			}
+			if(a/2<5000 & a >0)
+			{
+				System.out.println(a);
+				Num(2*a);
+				System.out.println(a);
+			}
+			if(a/2 > 5000 & a >0)
+			{
+				System.out.println(a/2);
+			}
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in =new Scanner(System.in);
+		System.out.println("请输入整数 n(0-5000)：");
+		int n = in.nextInt();
+		Num(n);
+	}
+}
